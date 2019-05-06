e19fc490ed28d2df3fbf22a1bb539328aa0cd667
astrapi69 <asterios.raptis@gmx.net>
2019-05-06 10:56:57 +0200

    Updates

---
+++ precache-manifest.81f158f2f0758e25e05dbf89f275b37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1552d73991febcf0cbc5348577de9802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73e02b64cf3c1a1b165",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d73e02b64cf3c1a1b165",
+    "url": "/static/js/main.7c025cb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
